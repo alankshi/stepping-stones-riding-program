4ad3f7e2e43e08f21b2eb9ca87485ee992037c0f
BinaryKiwiz <alan.kw.shi@gmail.com>
2024-05-23 14:32:54 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
@@ -10,4 +10,5 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
-})
+  base: "/stepping-stones-riding-program/",
+});
